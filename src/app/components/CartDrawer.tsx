6aa36eccde91f005496dfc8afb845ebf48eb4a89
Renan Martins <macbookair@MacBooks-Renan.local>
2024-11-04 02:23:13 -0300

    adicionando algo

--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -2,9 +2,15 @@
 import { useCartStore } from "@/store"
 import Image from "next/image";
 import { formatPrice } from "../lib/utils";
+import CheckoutButton from "./CheckoutButton";
+import Checkout from "./Checkout";
 
 export default function CartDrawer() {
     const useStore = useCartStore();
+
+    const totalPrice = useStore.cart.reduce((acc, item) => {
+        return acc + item.price! * item.quantity!;
+    }, 0);
 
     return (
         <div onClick={() => useStore.toggleCart()} className="fixed w-full h-screen bg-black/25 left-0 top-0 z-50">
@@ -13,6 +19,9 @@ export default function CartDrawer() {
             Voltar para loja
         </button>
         <div className="border-t border-gray-400 my-4"></div>
+
+        {useStore.onCheckout === 'cart' && (
+        <>
         {useStore.cart.map((item) => (
             <div key={item.id} className="flex gap-4 py-4">
                 <Image
@@ -40,6 +49,16 @@ export default function CartDrawer() {
                 </div>
             </div>
         ))}
+        </>
+        )};
+
+        {useStore.cart.length > 0 && useStore.onCheckout === 'cart' && (
+             <CheckoutButton totalPrice={totalPrice} />   
+            )}
+
+            {useStore.onCheckout === 'checkout' && (
+                <Checkout />
+            )}
         </div>
     </div>
     )
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+    publicRoutes: ['/', '/product(.*', '/sign-in', '/sign-up']
+});
 
 export const config = {
   matcher: [
